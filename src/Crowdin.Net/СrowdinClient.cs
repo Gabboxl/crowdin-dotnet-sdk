59cb75cb29905a9aeabba268da11c2925891059c
Gabriele <gabboxl0@gmail.com>
2024-03-13 22:24:27 +0100

    Remove /content from URL build

--- src/Crowdin.Net/СrowdinClient.cs
+++ src/Crowdin.Net/СrowdinClient.cs
@@ -66,7 +66,7 @@ namespace Crowdin.Net
             if (!crowdinResFilePath.StartsWith("/"))
                 crowdinResFilePath = $"/{crowdinResFilePath}";
             
-            var url = $"{_mBaseUrl}/content{crowdinResFilePath}";
+            var url = $"{_mBaseUrl}{crowdinResFilePath}";
             HttpResponseMessage response = await HttpClient.GetAsync(url);
             
             using Stream rawResponseStream = await response.Content.ReadAsStreamAsync();
